879acbc8a21c5ec863fd83115e0242e25c7103b9
Yuriy Taras <ytaras@softserveinc.com>
2018-11-29 15:12:47 +0200

    A bit better API

--- src/systems/validation.rs
+++ src/systems/validation.rs
@@ -8,12 +8,20 @@ trait Validation<'a> {
     type Output;
     type SD: SystemData<'a>;
 
-    fn run(&mut self, i: Self::Input, data: Self::SD) -> Self::Output;
+    fn run(&self, i: Self::Input, data: Self::SD) -> Self::Output;
 
-    fn exec(&mut self, i: Self::Input, world: &'a mut World) -> Self::Output {
+    fn exec(&self, i: Self::Input, world: &'a mut World) -> Self::Output {
         world.exec(|sd|
             self.run(i, sd)
         )
+    }
+
+    fn cons<V2>(&'a self, other: &'a V2) -> ConsV<Self, V2>
+        where V2: Validation<'a, Input=Self::Output>, Self: Sized {
+        ConsV {
+            v1: &self,
+            v2: other,
+        }
     }
 }
 
@@ -23,23 +31,34 @@ struct PureValidator<I, O> {
     od: PhantomData<O>,
 }
 
+impl<I, O> PureValidator<I, O> {
+    pub fn new(func: Box<Fn(I) -> O>) -> Self {
+        PureValidator { func, id: Default::default(), od: Default::default() }
+    }
+
+    pub fn from_closure<F>(f: F) -> Self where F: Fn(I) -> O + 'static {
+        let b = Box::new(f);
+        Self::new(b)
+    }
+}
+
 impl<'a, I, O> Validation<'a> for PureValidator<I, O> {
     type Input = I;
     type Output = O;
     type SD = ();
 
-    fn run(&mut self, i: <Self as Validation<'a>>::Input, (): <Self as Validation<'a>>::SD) -> <Self as Validation<'a>>::Output {
+    fn run(&self, i: <Self as Validation<'a>>::Input, (): <Self as Validation<'a>>::SD) -> <Self as Validation<'a>>::Output {
         let f = &self.func;
         f(i)
     }
 }
 
-struct ConsV<V1, V2> {
-    v1: V1,
-    v2: V2,
+struct ConsV<'a, V1, V2> where V1: 'a, V2: 'a {
+    v1: &'a V1,
+    v2: &'a V2,
 }
 
-impl<'a, V1, V2, I, IO, O> Validation<'a> for ConsV<V1, V2>
+impl<'a, V1, V2, I, IO, O> Validation<'a> for ConsV<'a, V1, V2>
     where V1: Validation<'a, Input=I, Output=IO> + 'a,
           V2: Validation<'a, Input=IO, Output=O> + 'a,
 {
@@ -50,7 +69,7 @@ impl<'a, V1, V2, I, IO, O> Validation<'a> for ConsV<V1, V2>
         <V2 as Validation<'a>>::SD,
     );
 
-    fn run(&mut self, i: <Self as Validation<'a>>::Input, (v1sd, v2sd): <Self as Validation<'a>>::SD) -> <Self as Validation<'a>>::Output {
+    fn run(&self, i: <Self as Validation<'a>>::Input, (v1sd, v2sd): <Self as Validation<'a>>::SD) -> <Self as Validation<'a>>::Output {
         let r1: IO = self.v1.run(i, v1sd);
         let r2: O = self.v2.run(r1, v2sd);
         r2
@@ -71,11 +90,9 @@ mod test {
     #[test]
     fn pure_validation() {
         let mut w = World::new();
-        let mut validator = PureValidator {
-            func: Box::new(|x: i32| x.to_string()),
-            id: Default::default(),
-            od: Default::default(),
-        };
+        let validator = PureValidator::from_closure(
+            |x: i32| x.to_string()
+        );
 
         let res = validator.exec(5, &mut w);
         assert_eq!("5", res);
@@ -84,21 +101,11 @@ mod test {
     #[test]
     fn cons_validation() {
         let mut w = World::new();
-        let validator1 = PureValidator {
-            func: Box::new(|x: i32| x as f32 + 0.1),
-            id: Default::default(),
-            od: Default::default(),
-        };
-        let validator2 = PureValidator {
-            func: Box::new(|x: f32| x.to_string()),
-            id: Default::default(),
-            od: Default::default(),
-        };
+        let validator1 = PureValidator::from_closure(|x: i32| x as f32 + 0.1);
 
-        let mut composite = ConsV {
-            v1: validator1,
-            v2: validator2,
-        };
+        let validator2 = PureValidator::from_closure(|x: f32| x.to_string());
+
+        let composite = validator1.cons(&validator2);
 
         let res = composite.exec(5, &mut w);
         assert_eq!("5.1", res);
