eefa63d01db346b37244af33f72a30d64d5e4932
Yuriy Taras <ytaras@softserveinc.com>
2018-11-29 11:52:34 +0200

    Using console instead api to draw in doryen

--- src/ui/mod.rs
+++ src/ui/mod.rs
@@ -1,34 +1,42 @@
-mod keymapper;
-use self::keymapper::*;
-use data::components::*;
-use data::structures::*;
 use doryen_rs::*;
 use specs::prelude::*;
+
+use data::components::*;
+use data::structures::*;
 use systems::logic::*;
 use systems::render::render_doryen;
+
+use self::keymapper::*;
+
+mod keymapper;
 
 pub struct GameWorld {
     world: World,
     key_mapper: KeyMapper,
     game_command_handler: GameCommandHandler,
+    console: Console,
 }
 
 impl GameWorld {
-    pub fn new(world: World) -> Self {
+    pub fn new(mut world: World) -> Self {
         let key_mapper = KeyMapper::new();
         let game_command_handler = GameCommandHandler;
+        let console: Console = world.exec(|level_info: Read<LevelInfo>| {
+            Console::new(level_info.width().into(), level_info.height().into())
+        });
         GameWorld {
             world,
             key_mapper,
             game_command_handler,
+            console,
         }
     }
 }
 
 impl Engine for GameWorld {
-    fn update(&mut self, _api: &mut DoryenApi) {
+    fn update(&mut self, api: &mut DoryenApi) {
         use specs::RunNow;
-        let input = _api.input();
+        let input = api.input();
         for (key, command) in self.key_mapper.commands() {
             if input.key_pressed(key) {
                 self.game_command_handler.exec(command, &mut self.world);
@@ -39,7 +47,7 @@ impl Engine for GameWorld {
         self.world.maintain();
     }
     fn render(&mut self, api: &mut DoryenApi) {
-        let mut renderer = render_doryen(api);
+        let mut renderer = render_doryen(api.con());
         use specs::RunNow;
         renderer.run_now(&self.world.res);
     }
